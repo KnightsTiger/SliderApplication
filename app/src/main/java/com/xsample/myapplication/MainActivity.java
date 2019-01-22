cc521e06d3f858e9b7f705b8fecc3bee9337f165
knight <tocaldera6734@gmail.com>
2019-01-22 11:59:33 +0530

    Created sample project and added one layout file with the name of slide

---
+++ app/src/main/java/com/xsample/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xsample.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView;
+        textView = findViewById(R.id.hi);
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
+    }
+}
